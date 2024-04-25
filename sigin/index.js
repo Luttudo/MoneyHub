8ea447f0c9aa7a7a886074bf89b2fce332b40699
Engu Luttudo <91648695+Luttudo@users.noreply.github.com>
2024-04-25 18:27:50 -0300

    tudo errado pfvr me mata

--- sigin/index.js
+++ sigin/index.js
@@ -1,22 +1,16 @@
-import { initializeApp } from "@firebase/app";
-import { getAuth } from "@firebase/auth";
+function signIn() {
+  const email = document.getElementById("floatingInput").value;
+  const password = document.getElementById("floatingPassword").value;
 
-const firebaseApp = initializeApp({
-
-    apiKey: "AIzaSyAva5fyh1j5_SnLZVYu3F1EnW7lh4YPSTo",
-  
-    authDomain: "moneyhub-f2246.firebaseapp.com",
-  
-    projectId: "moneyhub-f2246",
-  
-    storageBucket: "moneyhub-f2246.appspot.com",
-  
-    messagingSenderId: "611591284009",
-  
-    appId: "1:611591284009:web:cb7829aa12bba14b41c338",
-  
-    measurementId: "G-FD8648FGE2"
-  
-  });
-
-const auth = getAuth(firebaseApp);
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Usuário autenticado com sucesso
+      const user = userCredential.user;
+      console.log("Usuário autenticado:", user);
+      // Redirecione para a página desejada
+    })
+    .catch((error) => {
+      // Trate erros de autenticação
+      console.error("Erro ao autenticar:", error.message);
+    });
+}
